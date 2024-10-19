6a5ad3fd3d2adccec6ab2d88d05ed36c96d872dd
ChrisK1504 <krismehilli@yahoo.com>
2024-10-19 14:12:02 +0200

    Integrated the newly created CashCardRepository into the functionality of the Controller; Also created a SQL table and populated it with test data;

--- src/main/java/com/kmehilli/cashcard/CashCardController.java
+++ src/main/java/com/kmehilli/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package com.kmehilli.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,16 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
+
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
+	private final CashCardRepository cashCardRepository;
+
+	private CashCardController(CashCardRepository cashCardRepository) {
+		this.cashCardRepository = cashCardRepository;
+	}
+
 	@GetMapping("/{id}")
-	private ResponseEntity<CashCard> findById(@PathVariable Long id) {
-		if (id.equals(99L)) {
-			CashCard cashCard = new CashCard(99L, 123.45);
-			return ResponseEntity.ok(cashCard);
+	public ResponseEntity<CashCard> findById(@PathVariable Long id) {
+		Optional<CashCard> cashCardOptional = cashCardRepository.findById(id);
+		if (cashCardOptional.isPresent()) {
+			return ResponseEntity.ok(cashCardOptional.get());
 		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
+	
 }
