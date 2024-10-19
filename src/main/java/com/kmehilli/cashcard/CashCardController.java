615118b7d3e6d437a78549d7884bfd76b9dece28
ChrisK1504 <krismehilli@yahoo.com>
2024-10-19 15:19:36 +0200

    Created a test for a new "/cashcards" GET endpoint which should return all cash cards, while using pagination; Implemented this endpoint in the controller; Extended the CashCardRepository interface with the PagingAndSortingRepository;

--- src/main/java/com/kmehilli/cashcard/CashCardController.java
+++ src/main/java/com/kmehilli/cashcard/CashCardController.java
@@ -3,15 +3,17 @@ package com.kmehilli.cashcard;
 import java.net.URI;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -35,8 +37,9 @@ class CashCardController {
 	}
 
 	@GetMapping()
-	private ResponseEntity<Iterable<CashCard>> findAll() {
-		return ResponseEntity.ok(cashCardRepository.findAll());
+	private ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {
+		Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));
+		return ResponseEntity.ok(page.getContent());
 	}
 	
 	@PostMapping()
