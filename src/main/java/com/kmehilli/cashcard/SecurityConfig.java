474bf54ccc80d7d5c10b57d2aed7ea8d672bed8e
ChrisK1504 <krismehilli@yahoo.com>
2024-10-19 19:43:05 +0200

    Added Kumar to the UserDetails to prepare for the implementation of the final operation, Delete;

--- src/main/java/com/kmehilli/cashcard/SecurityConfig.java
+++ src/main/java/com/kmehilli/cashcard/SecurityConfig.java
@@ -44,7 +44,12 @@ class SecurityConfig {
                 .password(passwordEncoder.encode("qrs456"))
                 .roles("NON-OWNER")
                 .build();
-        return new InMemoryUserDetailsManager(sarah, hankWithNoCards);
+        UserDetails kumar = users
+                .username("kumar2")
+                .password(passwordEncoder.encode("xyz789"))
+                .roles("CARD-OWNER")
+                .build();
+        return new InMemoryUserDetailsManager(sarah, hankWithNoCards, kumar);
     }
 
 }
