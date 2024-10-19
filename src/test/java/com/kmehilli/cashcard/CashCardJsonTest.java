0361c06702da84026739dc0d6442394c4c4a80b7
ChrisK1504 <krismehilli@yahoo.com>
2024-10-19 14:49:44 +0200

    Added Serialization and Deserialization tests for the "list.json";

--- src/test/java/com/kmehilli/cashcard/CashCardJsonTest.java
+++ src/test/java/com/kmehilli/cashcard/CashCardJsonTest.java
@@ -25,8 +25,13 @@ public class CashCardJsonTest {
   void setUp() {
     cashCards = Arrays.array(
       new CashCard(99L, 123.45),
-      new CashCard(100L, 100.00),
+      new CashCard(100L, 1.00),
       new CashCard(101L, 150.00));
+  }
+
+  @Test
+  void cashCardListSerializationTest() throws IOException {
+    assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
   }
 
   @Test
@@ -39,6 +44,18 @@ public class CashCardJsonTest {
     assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
     assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
             .isEqualTo(123.45);
+  }
+
+  @Test
+  void cashCardListDeserializationTest() throws IOException {
+    String expected = """
+        [
+          { "id": 99, "amount": 123.45 },
+          { "id": 100, "amount": 1.00 },
+          { "id": 101, "amount": 150.00 }
+        ]
+        """;
+    assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
   }
 
   @Test
