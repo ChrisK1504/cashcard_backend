3ac58d825c2686b8a3a837f0ca966751ccd121a8
ChrisK1504 <krismehilli@yahoo.com>
2024-10-19 17:07:25 +0200

    Some extension changing the tab spaces;

--- src/main/java/com/kmehilli/cashcard/CashCard.java
+++ src/main/java/com/kmehilli/cashcard/CashCard.java
@@ -1,4 +1,8 @@
 package com.kmehilli.cashcard;
+
 import org.springframework.data.annotation.Id;
 
-record CashCard(@Id Long id, Double amount, String owner) {}
+record CashCard(@Id
+        Long id, Double amount, String owner) {
+
+}
